a27b9c4d903b080d048abeaa5858801f19efb69d
Wang, Daoxing <wang264@illinois.edu>
2021-03-13 20:31:08 -0500

    ccc

---
+++ bakelai_csv/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
+def solution(files):
+    # files - any of available files, i.e:
+    # files = ["./data/framp.csv", "./data/gnyned.csv", "./data/gwoomed.csv",
+    #            "./data/hoilled.csv", "./data/plent.csv", "./data/throwsh.csv",
+    #            "./data/twerche.csv", "./data/veeme.csv"]
+
+    # write your solution here
+    pass
